81136089f6817f6cdde1944470784ad94cc892cc
LeandroMore1 <manumore42@gmail.com>
2024-10-05 21:38:58 +0000

    Resolucion punto D

---
+++ RTA_SCRIPT_Examen_20241005/Punto_D.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "creando directorios de estructura asimetrica..."
+
+sudo mkdir -p /Estructura_Asimetrica/{correo/{carteros_{1..10},cartas_{1..100}},clientes/cartas_{1..100}}
+
+echo "mostrando directorios: "
+
+tree /Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
+
+ls /
