d955374885d54b213badafe5ee1e072d6a54d7c3
LeandroMore1 <manumore42@gmail.com>
2024-10-05 20:09:13 +0000

    realizacion punto A (estructuras simétricas)

---
+++ RTA_SCRIPT_Examen_20241005/Punto_A.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "creando carpetas..."
+
+sudo mkdir -p /Examenes-UTN/{alumno_1/parcial_{1..3},alumno_2/parcial_{1..3},alumno_3/parcial_{1..3},profesores}
+
+echo "carpeta creada satisfactoriamente"
