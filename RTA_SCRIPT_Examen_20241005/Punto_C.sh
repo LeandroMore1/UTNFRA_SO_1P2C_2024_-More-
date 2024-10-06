0e3e9cc2249c631ee43848eceb5a69d9cf8880f5
LeandroMore1 <manumore42@gmail.com>
2024-10-06 17:58:31 +0000

    realizacion parcial punto C

--- RTA_SCRIPT_Examen_20241005/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_C.sh
@@ -3,6 +3,10 @@ GRUPO_ALUMNOS=p1c2_2024_gAlumno
 GRUPO_PROFESORES=p1c2_2024_gProfesores
 VALIDAR_GRUPO_ALUMNOS=$(sudo cat /etc/group | grep -i 'p1c2_2024_gAlumno')
 VALIDAR_GRUPO_PROFESORES=$(sudo cat /etc/group | grep -i 'p1c2_2024_gProfesores')
+USERA1=$(sudo cat /etc/shadow | grep 'p1c2_2024_A1'| awk -F ':' '{print $1}')
+USERA2=$(sudo cat /etc/shadow | grep 'p1c2_2024_A2'| awk -F ':' '{print $1}')
+USERA3=$(sudo cat /etc/shadow | grep 'p1c2_2024_A3'| awk -F ':' '{print $1}')
+USERP1=$(sudo cat /etc/shadow | grep 'p1c2_2024_P1' | awk -F ':' '{print $1}')
 
 if [ -n "$VALIDAR_GRUPO_ALUMNOS" ]; then
     echo "el grupo alumnos ya existe"
@@ -11,6 +15,7 @@ else
     sudo groupadd $GRUPO_ALUMNOS
 fi
 
+
 if [ -n "$VALIDAR_GRUPO_PROFESORES" ]; then
     echo "el grupo profesores ya existe"
 else
@@ -18,8 +23,31 @@ else
     sudo groupadd $GRUPO_PROFESORES
 fi
 
-#sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_ALUMNOS p1c2_2024_A1
-#sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_ALUMNOS p1c2_2024_A2
-#sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_ALUMNOS p1c2_2024_A3
-#sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_PROFESORES p1c2_2024_P1 
+
+if [ -z "$USERA1" ]; then
+    sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_ALUMNOS p1c2_2024_A1
+else
+    echo "el user $USERA1 ya existe"
+fi	
+
+if [ -z "$USERA2" ]; then
+    sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_ALUMNOS p1c2_2024_A2
+else
+    echo "el user $USERA2 ya existe"
+fi
+
+if [ -z "$USERA3" ]; then
+    sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_ALUMNOS p1c2_2024_A3
+else
+    echo "el user $USERA3 ya existe"
+fi
+
+if [ -z "$USERP1" ]; then
+    sudo useradd -m -p "$PASS" -s /bin/bash -G $GRUPO_PROFESORES p1c2_2024_P1
+else
+    echo "el user $USERP1 ya existe"
+fi
+
+
+
 
