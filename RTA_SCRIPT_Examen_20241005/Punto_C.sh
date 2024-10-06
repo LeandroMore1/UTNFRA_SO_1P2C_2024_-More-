b38727c61e907e90d2ddfb90241eacd1fa6b2d25
LeandroMore1 <manumore42@gmail.com>
2024-10-06 20:56:33 +0000

    resolucion completa del punto C

--- RTA_SCRIPT_Examen_20241005/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_C.sh
@@ -48,6 +48,29 @@ else
     echo "el user $USERP1 ya existe"
 fi
 
+sudo chown -R p1c2_2024_A1 /Examenes-UTN/alumno_1
+sudo chown -R p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chown -R p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chown -R p1c2_2024_P1 /Examenes-UTN/profesores
 
+sudo chmod -R 750 p1c2_2024_A1 /Examenes-UTN/alumno_1
+sudo chmod -R 760 p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chmod -R 700 p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chmod -R 775 p1c2_2024_P1 /Examenes-UTN/profesores
 
+USUARIO1=p1c2_2024_A1:alumno_1
+USUARIO2=p1c2_2024_A2:alumno_2
+USUARIO3=p1c2_2024_A3:alumno_3
+USUARIO4=p1c2_2024_P1:profesores
+
+echo "creando archivo validar.txt para los usuarios..."
+
+for i in $USUARIO1 $USUARIO2 $USUARIO3 $USUARIO4
+do
+	USUARIO_ACTUAL=$(echo "$i" | awk -F ':' '{print $1}')
+	CARPETA_ACTUAL=$(echo "$i" | awk -F ':' '{print $2}')
+
+	sudo -u "$USUARIO_ACTUAL" bash -c "echo \$(whoami) > /Examenes-UTN/$CARPETA_ACTUAL/validar.txt"
+
+done
 
